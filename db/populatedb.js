1492a4f8c4a6bcda6a8241aeecd0c39dee698692
rndall <96964503+rndall@users.noreply.github.com>
2025-11-27 16:10:02 +0800

    feat: create sql for `populatedb.js`

--- db/populatedb.js
+++ db/populatedb.js
@@ -2,7 +2,26 @@
 
 import { Client } from "pg"
 
-const SQL = ``
+const SQL = `
+CREATE TABLE IF NOT EXISTS "user" (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password_hash TEXT NOT NULL,
+  is_member BOOLEAN DEFAULT FALSE,
+  is_admin BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS message (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id INTEGER NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  content TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`
 
 async function main() {
 	console.log("seeding...")
